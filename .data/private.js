1f60a31c8f6799a6fde245628c43db0cf9dafa5c
Fenthyr <alexanderlange90@gmail.com>
2025-12-01 12:45:29 +0100

    Create private.js

---
+++ .data/private.js
@@ -0,0 +1,5 @@
+// Dummy-Konfiguration für Render – ersetzt die alte Glitch-Datei
+module.exports = {
+  // hier könnten eigentlich geheime Daten / Build-Infos stehen
+  // Für die normale Item-Ansicht reicht ein leerer Export.
+};
